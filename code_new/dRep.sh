a4d3d0cabe33649a641d74e0f7f360472b1c4f88
bosco <boscogracia97@gmail.com>
2024-08-27 17:23:56 +0200

    Add code folder with scripts, excluding specified folders

---
+++ code_new/dRep.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# This script collects all the metagenomic and isolate genomes and de-replicates them, keeping only one representative for each taxon.
+# https://drep.readthedocs.io/en/latest/overview.html#a-drep-based-metagenomic-workflow
+# Bosco Gracia Alvira, 2023
+
+### VARIABLES
+WORKDIR="/Users/bgracia/Dropbox (PopGen)/Bosco/PhD_Dropbox/Ancestral_microbiome/data"
+ASSEMBLY="/Users/bgracia/Dropbox (PopGen)/Bosco/PhD_Dropbox/Isolates_assembly"
+TEMP="/Users/bgracia/PhD_local/temp"
+
+### COMMANDS
+if [ ! -d "$TEMP"/Genomes ]
+then
+    mkdir -p "$TEMP"/Genomes
+fi
+
+cp "$WORKDIR"/DeNovo_Assembly_*/06_MERGED/SUMMARY_FINAL/bin_by_bin/*/*.fa "$TEMP"/Genomes/
+cp "$ASSEMBLY"/Pool_???/07.GTDB-Tk/Genomes/*.fasta "$TEMP"/Genomes/
+
+for i in $(basename -a "$TEMP"/Genomes/*.fa);
+do
+    mv "$TEMP"/Genomes/${i} "$TEMP"/Genomes/${i}sta;
+done
+git add code/**/*.sh code/**/*.R code/**/*.Rmd
+eval "$(conda shell.bash hook)"
+conda activate drep-3.4
+export CHECKM_DATA_PATH=/Users/bgracia/PhD_local/db/CheckM
+
+# Note that I use a secondary alignment threshold of 98% because I want strain resolution.
+dRep dereplicate \
+    "$TEMP"/dRep \
+    -g "$TEMP"/Genomes/*.fasta \
+    --S_algorithm fastANI \
+    --multiround_primary_clustering \
+    -ms 10000 \
+    -pa 0.9 \
+    -sa 0.98 \
+    -nc 0.30 \
+    -cm larger \
+    -p 10
+
+conda deactivate
+
+eval "$(conda shell.bash hook)"
+conda activate gtdbtk-2.0
+export GTDBTK_DATA_PATH=/Users/bgracia/PhD_local/db/GTDB/release207
+
+mkdir "$TEMP"/GTDB-Tk
+
+for i in $(basename "$TEMP"/dRep/dereplicated_genomes/*.fasta);
+do  mkdir "$TEMP"/GTDB-Tk/${i%.fasta};
+    cp "$TEMP"/dRep/dereplicated_genomes/${i} "$TEMP"/GTDB-Tk/${i%.fasta}/${i}
+    gtdbtk classify_wf \
+                --genome_dir "$TEMP"/GTDB-Tk/${i%.fasta} \
+                -x fasta \
+                --cpus 8 \
+                --out_dir "$TEMP"/GTDB-Tk/${i%.fasta};
+    rm "$TEMP"/GTDB-Tk/${i%.fasta}/${i};
+done
+
+cat "$TEMP"/GTDB-Tk/*/gtdbtk.bac120.summary.tsv | sort -ur > "$TEMP"/GTDB-Tk/summary.tsv
+
+mv "$TEMP"/dRep "$WORKDIR"
+mv "$TEMP"/GTDB-Tk/summary.tsv "$WORKDIR"/dRep
+rm -r "$TEMP"
