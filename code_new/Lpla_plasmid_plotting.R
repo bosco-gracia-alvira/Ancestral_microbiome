12f6c38970d556a3840491b7cf62f45772459b20
bosco <boscogracia97@gmail.com>
2024-12-17 11:50:13 +0100

    Remove the "island" variable

--- code_new/Lpla_plasmid_plotting.R
+++ code_new/Lpla_plasmid_plotting.R
@@ -14,30 +14,34 @@ cov <- fread(paste0(cov_path,"coverage.tsv"), header=T)
 cov <- cov %>% 
         mutate(
           position = case_when(
-            gene == "island" ~ position-102018,
             gene == "recA" ~ position-102018,
-            gene == "plasmid" ~ position,
-          )
-        )
+            gene == "plasmid" ~ position)) %>% 
+          mutate(
+            source = if_else(grepl("^.F", sample), "pool", "isolate"))
 
 # Extract the coverage of recA
 recA_mean <- cov %>%
   filter(gene == "recA") %>% 
-  select(isolate,coverage) %>% 
-  group_by(isolate) %>% 
-  summarise(recA_mean = mean(coverage))
+  select(sample,coverage) %>% 
+  group_by(sample) %>% 
+  summarise(recA_mean = sum(coverage)/(1142)) # The value 1142 is the length of the recA gene
+
+### Isolates analysis
+
+# In this part of the analysis we want to check for the presence of the genomic island in each isolate
+cov_isolates <- cov %>% filter(source == "isolate")
 
 # Import the metadata of all the isolates and merge it with the coverage
 metadata_isolates <- fread(metadata_isolates_path, header=T)
-cov_merged <- merge(cov, metadata_isolates, by.x="isolate", by.y="sample")
-cov_merged <- merge(cov_merged, recA_mean, by="isolate")
+cov_merged <- merge(cov_isolates, metadata_isolates, by="sample")
+cov_merged <- merge(cov_merged, recA_mean, by="sample")
 cov_merged <- cov_merged %>% 
                 mutate(norm_coverage = coverage/recA_mean)
 
 # New genotype column, that matches the temperature, with three exceptions
 cov_merged <- cov_merged %>% 
                 mutate(Genotype = case_when(
-                    isolate %in% c("B25", "X76", "X371") ~ "S239",
+                    sample %in% c("B25", "X76", "X371") ~ "S239",
                     Temperature == "HOT" ~ "S239",
                     Temperature == "BASE" ~ "B89",
                     Temperature == "COLD" ~ "S103",
@@ -45,7 +49,6 @@ cov_merged <- cov_merged %>%
 
 # Subset the coverages from the plasmid and the housekeeping gene RecA
 plasmid <- cov_merged %>% filter(gene=="plasmid")
-island <- cov_merged %>% filter(gene=="island")
 recA <- cov_merged %>% filter(gene=="recA")
 
 # Plot and save the data :)
@@ -57,7 +60,7 @@ plot_cov_plasmid <- ggplot(plasmid, aes(x = position, y = coverage, colour = Gen
                         scale_colour_manual(values = c("S239" = "red", "B89" = "grey", "S103" = "lightblue")) +
                         geom_hline(aes(yintercept = recA_mean), linetype = "dashed", color = "black") +
                         scale_x_continuous(labels = scales::scientific) +
-                        facet_wrap(~ isolate, scales = "free_y") +
+                        facet_wrap(~ sample, scales = "free_y") +
                         labs(title = "Coverage of the plasmid in each the isolate", x = "Position", y = "Coverage") +
                         theme_minimal() +
                         theme(
@@ -71,18 +74,17 @@ ggsave(filename = "plot_cov_plasmid.png", plot = plot_cov_plasmid, path = visual
 
 # Coverage of the whole plasmid for all the isolates
 plot_cov_plasmid_all <- ggplot(plasmid, aes(x = position, y = coverage, colour = Genotype)) +
-  annotate("rect", xmin = 102018, xmax = 184830, ymin = 1, ymax = Inf, alpha = 0.2, fill = "lightgreen") +
-  geom_point(size=0.03) +
-  #scale_y_continuous(trans='log10') +
-  scale_colour_manual(values = c("S239" = "red", "B89" = "grey", "S103" = "lightblue")) +
-  scale_x_continuous(labels = scales::scientific) +
-  labs(title = "Overlapped coverages of the plasmid in all the isolates", x = "Position", y = "Coverage") +
-  theme_minimal() +
-  theme(
-    axis.text.x = element_text(size = 10, angle = 45, hjust = 1),
-    axis.text.y = element_text(size = 10),
-    axis.title.x = element_text(size = 14),
-    axis.title.y = element_text(size = 14))
+                            annotate("rect", xmin = 102018, xmax = 184830, ymin = 1, ymax = Inf, alpha = 0.2, fill = "lightgreen") +
+                            geom_point(size=0.03) +
+                            scale_colour_manual(values = c("S239" = "red", "B89" = "grey", "S103" = "lightblue")) +
+                            scale_x_continuous(labels = scales::scientific) +
+                            labs(title = "Overlapped coverages of the plasmid in all the isolates", x = "Position", y = "Coverage") +
+                            theme_minimal() +
+                            theme(
+                              axis.text.x = element_text(size = 10, angle = 45, hjust = 1),
+                              axis.text.y = element_text(size = 10),
+                              axis.title.x = element_text(size = 14),
+                              axis.title.y = element_text(size = 14))
 
 plot_cov_plasmid_all
 
@@ -94,7 +96,7 @@ plot_cov_recA <- ggplot(recA, aes(x = position, y = coverage, colour = Genotype)
                     #scale_y_continuous(trans='log10') +
                     scale_colour_manual(values = c("S239" = "red", "B89" = "grey", "S103" = "lightblue")) +
                     geom_hline(aes(yintercept = recA_mean), linetype = "dashed", color = "black") +
-                    facet_wrap(~ isolate) +
+                    facet_wrap(~ sample) +
                     labs(title = "RecA coverage per isolate", x = "Position", y = "Coverage") +
                     theme_minimal() +
                     theme(
@@ -112,7 +114,7 @@ plot_cov_norm <- ggplot(plasmid, aes(x = position, y = norm_coverage, colour = G
                     geom_point(size=0.03) +
                     scale_colour_manual(values = c("S239" = "red", "B89" = "grey", "S103" = "lightblue")) +
                     scale_x_continuous(labels = scales::scientific) +
-                    facet_wrap(~ isolate, scales = "free_y") +
+                    facet_wrap(~ sample, scales = "free_y") +
                     labs(title = "Normalised coverage of the plasmid in each the isolate", x = "Position", y = "Normalised coverage") +
                     theme_minimal() +
                     theme(
@@ -124,6 +126,53 @@ plot_cov_norm
 
 ggsave(filename = "plot_cov_norm.png", plot = plot_cov_norm, path = visuals_path, width = 24, height = 12, dpi = 300)
 
+
+### Pools analysis
+
+# In this part we want to study the plasmid dynamics over the generations
+cov_pools <- cov %>% filter(source == "pool")
+
+cov_pools_meta <- cov_pools %>% 
+                      mutate(
+                        generation = as.numeric(str_extract(sample, "(?<=F)\\d+")),
+                        replicate = str_extract(sample, "(?<=_)\\w+$"),
+                        temperature = case_when(
+                          str_detect(sample, "h") ~ "hot",
+                          str_detect(sample, "c") ~ "cold"))
+
+cov_summed_pools <- cov_pools_meta %>%
+                        mutate(sample = str_extract(sample, "^.F\\d+")) %>% 
+                        group_by(position,sample,gene,generation,temperature) %>% 
+                        summarize(total_coverage = sum(coverage), .groups = 'drop')
+
+summed_recA <- cov_summed_pools %>%
+  filter(gene == "recA") %>% 
+  select(sample,total_coverage) %>% 
+  group_by(sample) %>% 
+  summarise(recA_mean = sum(total_coverage)/(1142)) # The value 1142 is the length of the recA gene
+
+cov_summed_pools_plasmid <- cov_summed_pools %>%  
+                                    filter(gene=="plasmid") %>% 
+                                    left_join(summed_recA, by="sample") %>% 
+                                    mutate(recA_mean = replace_na(recA_mean, 0))
+
+plot_cov_pools <- ggplot(cov_summed_pools_plasmid, aes(x = position, y = total_coverage, colour = temperature)) +
+                      annotate("rect", xmin = 102018, xmax = 184830, ymin = 1, ymax = Inf, alpha = 0.2, fill = "lightgreen") +
+                      geom_point(size=0.03) +
+                      geom_hline(aes(yintercept = recA_mean), linetype = "dashed", color = "black") +
+                      scale_colour_manual(values = c("hot" = "red", "cold" = "lightblue")) +
+                      scale_x_continuous(labels = scales::scientific) +
+                      facet_wrap(~ sample, scales = "free_y") +
+                      labs(title = "Plasmid coverage per generation", x = "Position", y = "Normalised coverage") +
+                      theme_minimal() +
+                      theme(
+                        axis.text.x = element_text(size = 10, angle = 45, hjust = 1),
+                        axis.text.y = element_text(size = 10),
+                        axis.title.x = element_text(size = 14),
+                        axis.title.y = element_text(size = 14))
+plot_cov_pools
+
+ggsave(filename = "plot_cov_pools.png", plot = plot_cov_pools, path = visuals_path, width = 24, height = 12, dpi = 300)
 
 
 
