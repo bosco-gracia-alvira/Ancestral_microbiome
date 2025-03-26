11a1e0c9af07b8c22a8bc86560273120cf1fc10f
Bosco <boscogracia97@gmail.com>
2025-03-26 15:35:35 +0100

    Modified the whole code

--- code_new/Lpla_plasmid_plotting.R
+++ code_new/Lpla_plasmid_plotting.R
@@ -1,16 +1,26 @@
-library(data.table)
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(tidyverse)
-library(stringr)
-library(gggenomes)
-library(gtools)
+# Standard table management
+library(data.table) ; packageVersion("data.table")
+library(dplyr) ; packageVersion("dplyr")
+library(tidyr) ; packageVersion("tidyr")
+library(tidyverse) ; packageVersion("tidyverse")
+library(stringr) ; packageVersion("stringr")
+
+# Graph visualisation
+library(ggplot2) ; packageVersion("ggplot2")
+library(gggenomes) ; packageVersion("gggenomes")
+library(gtools) ; packageVersion("gtools")
+library(aplot) ; packageVersion("aplot")
+
+# Phylogenetic tree visualisation
+library(treeio) ; packageVersion("treeio")
+library(ggtree) ; packageVersion("ggtree")
+library(phytools) ; packageVersion("phytools")
 
 # Paths to the files that we want to import (and export)
 cov_path <- "/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/Ancestral_microbiome/data/Lpla_plasmid"
 gff_path <- normalizePath(paste0(cov_path,"/reference/LplaWF.gff"))
 fa_path <- normalizePath(paste0(cov_path,"/reference/LplaWF.fa"))
+tree_path <- normalizePath(paste0(cov_path,"/tree/Lpla.phylip.treefile"))
 metadata_isolates_path <- "/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/Ancestral_microbiome/data/SNPs_analysis/metadata.tsv"
 visuals_path <- "/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/Ancestral_microbiome/visuals/Lpla_plasmid"
 
@@ -19,8 +29,8 @@ visuals_path <- "/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/An
 ####################################################################
 
 # Read the plasmid fasta file in order to get the length
-fa <- read_seq_len(fa_path) %>% 
-  filter(seq_id=="contig_2")
+fa <- read_seq_len(fa_path) %>%
+            filter(seq_id=="contig_2")
 
 # Read the gff file of the plasmid and modify it to fit gggenomes
 gff <- read_gff3(gff_path) %>%
@@ -44,10 +54,52 @@ island <- data.frame(
   Name = "Island",
   colour = "green")
 
-# Import the isolates' metadata and coverage data and set the position in the beginning of each genomic element
-metadata_isolates <- fread(metadata_isolates_path, header=T) %>% 
-  select(c(sample,Temperature,Generation,Population,Replicate))
+# Plot the genetic map
+# Create a gggenomes object that contains all the genetic features
+genmap <- gggenomes(seqs= fa,feats = gff) %>% 
+                          add_feats(island)
 
+# Plot the genetic map
+genmap_plot <- genmap +
+  geom_feat(data = feats(island), aes(colour=colour), alpha = 0.2, position="pile", linewidth = 9) +
+  scale_colour_identity() +
+  labs(x = "Position") +
+  geom_seq() +
+  geom_gene(aes(fill = colour), shape = c(7, 4), intron_types = c("CDS","tRNA"), size = 5) +
+  scale_fill_identity() +
+  theme(
+    axis.text.x = element_text(size = 20),
+    axis.title.x = element_text(size = 20)
+  )
+
+# Read the tree and re-root it to the midpoint
+tree <- read.tree(tree_path)
+tree <- ladderize(tree, right = FALSE)
+tree <- midpoint.root(tree)
+
+# Plot the tree
+tree_plot <- ggtree(tree, size = 1)
+# tree_plot + geom_text(aes(label = node), hjust = -0.3) # This gives you the node numbers of the tree, in order to manipulate them
+tree_plot <- flip(tree_plot, 7, 8) # nodes 7 and 8 correspond to B246 and LpWF. We flip the branch for aesthetic reasons, so that LpWF is at the bottom of the tree.
+
+# Save the order of the genomes in the tree
+genome_order <- tree_plot$data %>%
+  filter(isTip) %>%
+  arrange(y) %>%
+  pull(label)
+
+# Import the isolates' metadata and coverage data and set the position in the beginning of each genomic element
+meta_LpWF <- data.frame(sample = "LpWF",
+                        Temperature = "NA",
+                        Generation = "NA",
+                        Population = "NA",
+                        Replicate = "NA")
+
+metadata_isolates <- fread(metadata_isolates_path, header=T) %>% 
+  select(c(sample,Temperature,Generation,Population,Replicate)) %>% 
+  rbind(meta_LpWF)
+
+# Import the coverage features of the plasmid in each genome
 cov_feats <- fread(paste0(cov_path,"/coverage.tsv"), header=T) %>% 
   filter(contig == "contig_2") %>%
   filter(coverage > 5) %>% 
@@ -60,38 +112,87 @@ cov_feats <- fread(paste0(cov_path,"/coverage.tsv"), header=T) %>%
   select(-c(contig,position)) %>% 
   mutate(colour = case_when(
     sample %in% c("B25", "X76", "X371") ~ "red",
+    sample %in% "LpWF" ~ "darkgrey",
     Temperature == "HOT" ~ "red",
     Temperature == "BASE" ~ "grey",
     Temperature == "COLD" ~ "lightblue",
     Temperature == "COLD-CONSTANT" ~ "lightblue")) %>% 
-  arrange(colour, levels(c("red", "lightblue", "grey")))
+  mutate(sample = factor(sample, levels = genome_order)) %>%
+  arrange(sample) %>% 
+  filter(!is.na(sample))
 
-# Create y_pos, which will map each sample to the y axis of the plot
-sample_mapping <- setNames(seq_along(unique(cov_feats$sample)), unique(cov_feats$sample))
-
-cov_feats <- cov_feats %>%
-  mutate(y_pos = sample_mapping[sample])
-
-# Create a gggenomes object that contains all the genetic features
-p1 <- gggenomes(seqs= fa,feats = gff) %>% 
-  add_feats(island) %>% 
-  add_feats(cov_feats)
-
-# Add the genetic map and the island range on the background
-p2 <- p1 +
-  geom_feat(data = feats(island), aes(colour=colour), alpha = 0.2, position="pile", linewidth = 9) +
+# Plot the coverage of the plasmid in each sample
+cov_plot <- ggplot(cov_feats) + 
+  geom_tile(aes(start, sample, fill=colour)) + 
+  scale_fill_identity() + 
   scale_colour_identity() +
-  labs(title = "Genetic map of the plasmid with Genetic Island", x = "Position") +
-  geom_seq() +
-  geom_gene(aes(fill = colour), shape = c(7, 4), intron_types = c("CDS","tRNA"), size = 5) +
-  scale_fill_identity() +
-  labs(title = "Genetic map of the plasmid", x = "Position") +
-  theme(
-    axis.text.x = element_text(size = 20),
-    axis.title.x = element_text(size = 20),
-  )
+  theme_minimal() +
+  theme_void() +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()) +
+  geom_text(aes(x = min(start) - 1000, y = sample, label = sample, color = colour), hjust = 1, size = 5)
 
-p2
+# Plot the combined figure
+combined_plot <- cov_plot %>% insert_left(tree_plot, width=0.1) %>% insert_bottom(genmap_plot, height=.1)
+
+ggsave(filename = "map_isolates_all.png", plot = combined_plot, path = visuals_path, width = 30, height = 12, dpi = 300)
+
+
+#######################################################################
+# DRAW THE GENETIC MAP OF THE PLASMID AND THE COVERAGE PER GENERATION #
+#######################################################################
+
+cov_pools <- fread(paste0(cov_path,"/coverage.tsv"), header=T) %>% 
+  filter(contig == "contig_2") %>%
+  filter(grepl("^.F", sample)) %>%
+  mutate(sample = str_extract(sample, "^.F\\d+")) %>% 
+  group_by(position,sample,contig) %>% 
+  summarize(total_coverage = sum(coverage), .groups = 'drop') %>% 
+  #filter(total_coverage > 5) %>%
+  mutate(start = position,
+         end = position,
+         seq_id = contig,
+         identity = 1) %>%
+  select(-contig) %>% 
+  mutate(
+    Generation = as.numeric(str_extract(sample, "(?<=F)\\d+")),
+    Temperature = case_when(
+      str_detect(sample, "h") ~ "hot",
+      str_detect(sample, "c") ~ "cold"),
+    colour = case_when(
+      str_detect(sample, "F0") ~ "grey",
+      str_detect(sample, "h") ~ "red",
+      str_detect(sample, "c") ~ "lightblue")) %>%
+  mutate(sample = factor(sample , levels = mixedsort(unique(sample)))) %>%
+  arrange(sample)
+
+# Plot the coverage of the plasmid in each sample
+pools_plot <- ggplot(cov_pools) + 
+  geom_tile(aes(start, sample, fill=colour)) + 
+  scale_fill_identity() + 
+  scale_colour_identity() +
+  theme_minimal() +
+  theme_void() +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()) +
+  geom_text(aes(x = min(start) - 1000, y = sample, label = sample, color = colour), hjust = 1, size = 5)
+
+# Plot the combined figure
+combined_pools <- pools_plot %>% insert_bottom(genmap_plot, height=.1)
+
+ggsave(filename = "map_pools.png", plot = combined_pools, path = visuals_path, width = 30, height = 12, dpi = 300)
+
+
+
+####################################
+
+
+
+
+
+
 
 # Add the coverage data from all the sequences
 p3 <- p2 + 
@@ -102,7 +203,6 @@ p3 <- p2 +
 p3
 
 ggsave(filename = "map_isolates_all.png", plot = p3, path = visuals_path, width = 20, height = 12, dpi = 300)
-
 
 # We subset one representative per plasmid class
 p4 <- p1
@@ -128,7 +228,6 @@ p5 <- p4 +
 p5
 
 ggsave(filename = "map_isolates_rep.png", plot = p5, path = visuals_path, width = 20, height = 12, dpi = 300)
-
 
 ### Pools analysis
 
@@ -196,14 +295,223 @@ t3
 ggsave(filename = "map_pools.png", plot = t3, path = visuals_path, width = 20, height = 12, dpi = 300)
 
 
+############################################
+
+
+tree_plot
+
+p2
 
 
 
 
 
+####################################################################
+# DRAW THE GENETIC MAP OF THE PLASMID AND THE COVERAGE PER ISOLATE #
+####################################################################
+
+# Read the plasmid fasta file in order to get the length
+fa <- read_seq_len(fa_path) %>% 
+  filter(seq_id=="contig_2")
+
+# Read the gff file of the plasmid and modify it to fit gggenomes
+gff <- read_gff3(gff_path) %>%
+  dplyr::filter(seq_id == "contig_2") %>% 
+  dplyr::select(seq_id, start, end, strand, type, name) %>%
+  dplyr::filter(!is.na(name)) %>%
+  dplyr::mutate(colour = case_when(
+    grepl("Transposase", name, ignore.case = TRUE) ~ "purple",
+    grepl("Mobile", name, ignore.case = TRUE) ~ "yellow",
+    grepl("tRNA-", name) ~ "brown",
+    TRUE ~ "grey"
+  ))
+
+# Read the tree, re-root it to the midpoint, plot it and save the order of the genomes
+tree <- read.tree(tree_path)
+tree <- ladderize(tree, right = FALSE)
+tree <- midpoint.root(tree)
+
+tree_plot <- ggtree(tree, size = 1)
+
+genome_order <- tree_plot$data %>%
+  filter(isTip) %>%
+  arrange(y) %>%
+  pull(label)
+
+ggsave(filename = "tree.png", plot = tree_plot, path = visuals_path, width = 10, height = 20, dpi = 300)
+
+# Create a new feature with the genetic island range
+island <- data.frame(
+  seq_id = "contig_2",
+  start = 102018,
+  end = 184830,
+  strand = "+",
+  type = "Island",
+  Name = "Island",
+  colour = "green")
+
+# Import the isolates' metadata and coverage data and set the position in the beginning of each genomic element
+meta_LpWF <- data.frame(sample = "LpWF",
+                        Temperature = "NA",
+                        Generation = "NA",
+                        Population = "NA",
+                        Replicate = "NA")
+
+metadata_isolates <- fread(metadata_isolates_path, header=T) %>% 
+  select(c(sample,Temperature,Generation,Population,Replicate)) %>% 
+  rbind(meta_LpWF)
+
+# Import the coverage features of the plasmid in each genome
+cov_feats <- fread(paste0(cov_path,"/coverage.tsv"), header=T) %>% 
+  filter(contig == "contig_2") %>%
+  filter(coverage > 5) %>% 
+  left_join(metadata_isolates, by=join_by(sample)) %>% 
+  filter(!is.na(Temperature)) %>% 
+  mutate(start = position,
+         end = position,
+         seq_id = contig,
+         identity = 1) %>%
+  select(-c(contig,position)) %>% 
+  mutate(sample = !is.na(sample)) %>% 
+  mutate(colour = case_when(
+    sample %in% c("B25", "X76", "X371") ~ "red",
+    sample %in% "LpWF" ~ "grey",
+    Temperature == "HOT" ~ "red",
+    Temperature == "BASE" ~ "grey",
+    Temperature == "COLD" ~ "lightblue",
+    Temperature == "COLD-CONSTANT" ~ "lightblue")) %>% 
+  mutate(sample = factor(sample, levels = genome_order)) %>%
+  arrange(sample)
+
+# Create y_pos, which will map each sample to the y axis of the plot
+sample_mapping <- setNames(seq_along(unique(cov_feats$sample)), unique(cov_feats$sample))
+
+cov_feats <- cov_feats %>%
+  mutate(y_pos = sample_mapping[sample])
+
+# Create a gggenomes object that contains all the genetic features
+p1 <- gggenomes(seqs= fa,feats = gff) %>% 
+  add_feats(island) %>% 
+  add_feats(cov_feats)
+
+# Add the genetic map and the island range on the background
+p2 <- p1 +
+  geom_feat(data = feats(island), aes(colour=colour), alpha = 0.2, position="pile", linewidth = 9) +
+  scale_colour_identity() +
+  labs(title = "Genetic map of the plasmid with Genetic Island", x = "Position") +
+  geom_seq() +
+  geom_gene(aes(fill = colour), shape = c(7, 4), intron_types = c("CDS","tRNA"), size = 5) +
+  scale_fill_identity() +
+  labs(title = "Genetic map of the plasmid", x = "Position") +
+  theme(
+    axis.text.x = element_text(size = 20),
+    axis.title.x = element_text(size = 20),
+  )
+
+p2
+
+# Add the coverage data from all the sequences
+p3 <- p2 + 
+  geom_segment(data = feats(cov_feats), aes(x = start, xend = start, y = 0.02*(y_pos), yend = 0.02*(y_pos + identity), colour = colour), linewidth = 1) +
+  geom_hline(data = feats(cov_feats), aes(yintercept = 0.02*(y_pos+1), colour = "black"), linewidth = 0.05) +
+  geom_text(data = feats(cov_feats), aes(x = -2000, y = 0.02*(y_pos+0.5), label = sample, colour = colour), hjust = 1, vjust = 0.5, size = 2)
+
+p3
+
+ggsave(filename = "map_isolates_all.png", plot = p3, path = visuals_path, width = 20, height = 12, dpi = 300)
+
+# We subset one representative per plasmid class
+p4 <- p1
+p4$data$feats[[3]] <- p4$data$feats[[3]] %>% 
+  filter(sample %in% c("B89", "B246", "S89", "X596")) 
+
+p5 <- p4 + 
+  geom_feat(data = feats(island), aes(colour=colour), alpha = 0.2, position="pile", linewidth = 9) +
+  scale_colour_identity() +
+  labs(title = "Genetic map of the plasmid with Genetic Island", x = "Position") +
+  geom_seq() +
+  geom_gene(aes(fill = colour), shape = c(7, 4), intron_types = c("CDS","tRNA"), size = 5) +
+  scale_fill_identity() +
+  labs(title = "Genetic map of the plasmid", x = "Position") +
+  theme(
+    axis.text.x = element_text(size = 20),
+    axis.title.x = element_text(size = 20)) +
+  geom_segment(data = feats(cov_feats), aes(x = start, xend = start, y = 0.7, yend = 0.8 * identity, colour = colour), linewidth = 1) +
+  geom_coverage(data = feats(cov_feats), aes(z = coverage, fill = colour), height = 0.8, offset = 0.3) +
+  scale_fill_identity() +
+  facet_wrap(~sample)
+
+p5
+
+ggsave(filename = "map_isolates_rep.png", plot = p5, path = visuals_path, width = 20, height = 12, dpi = 300)
+
+### Pools analysis
+
+# In this part we want to study the plasmid dynamics over the generations
+
+# Create a feature with the alignment to the pools
+cov_pools <- fread(paste0(cov_path,"/coverage.tsv"), header=T) %>% 
+  filter(contig == "contig_2") %>%
+  filter(grepl("^.F", sample)) %>%
+  mutate(sample = str_extract(sample, "^.F\\d+")) %>% 
+  group_by(position,sample,contig) %>% 
+  summarize(total_coverage = sum(coverage), .groups = 'drop') %>% 
+  #filter(total_coverage > 5) %>%
+  mutate(start = position,
+         end = position,
+         seq_id = contig,
+         identity = 1) %>%
+  select(-contig) %>% 
+  mutate(
+    Generation = as.numeric(str_extract(sample, "(?<=F)\\d+")),
+    Temperature = case_when(
+      str_detect(sample, "h") ~ "hot",
+      str_detect(sample, "c") ~ "cold"),
+    colour = case_when(
+      str_detect(sample, "F0") ~ "grey",
+      str_detect(sample, "h") ~ "red",
+      str_detect(sample, "c") ~ "lightblue")) %>%
+  mutate(sample = factor(sample , levels = mixedsort(unique(sample)))) %>%
+  arrange(sample)
+
+# Create y_pos, which will map each sample to the y axis of the plot
+sample_mapping <- setNames(seq_along(unique(cov_pools$sample)), unique(cov_pools$sample))
+
+cov_pools <- cov_pools %>%
+  mutate(y_pos = sample_mapping[sample])
 
 
+t1 <- gggenomes(seqs= fa,feats = gff) %>% 
+  add_feats(island) %>% 
+  add_feats(cov_pools)
 
+# Add the genetic map and the island range on the background
+t2 <- t1 +
+  geom_feat(data = feats(island), aes(colour=colour), alpha = 0.2, position="pile", linewidth = 9) +
+  scale_colour_identity() +
+  labs(title = "Genetic map of the plasmid with Genetic Island", x = "Position") +
+  geom_seq() +
+  geom_gene(aes(fill = colour), shape = c(7, 4), intron_types = c("CDS","tRNA"), size = 5) +
+  scale_fill_identity() +
+  labs(title = "Genetic map of the plasmid", x = "Position") +
+  theme(
+    axis.text.x = element_text(size = 20),
+    axis.title.x = element_text(size = 20))
+
+t2
+
+# Add the coverage data from all the sequences
+t3 <- t2 + 
+  geom_segment(data = feats(cov_pools), aes(x = start, xend = start, y = 0.02*(y_pos), yend = 0.02*(y_pos + identity), colour = colour), linewidth = 1) +
+  geom_hline(data = feats(cov_pools), aes(yintercept = 0.02*(y_pos+1), colour = "black"), linewidth = 0.05) +
+  geom_text(data = feats(cov_pools), aes(x = -2000, y = 0.02*(y_pos+0.5), label = sample, colour = colour), hjust = 1, vjust = 0.5, size = 2)
+
+t3
+
+ggsave(filename = "map_pools.png", plot = t3, path = visuals_path, width = 20, height = 12, dpi = 300)
+
+
+############################################
 
 
 
@@ -255,11 +563,6 @@ plot_cov_pools <- ggplot(cov_summed_pools_plasmid, aes(x = position, y = total_c
 plot_cov_pools
 
 ggsave(filename = "plot_cov_pools.png", plot = plot_cov_pools, path = visuals_path, width = 24, height = 12, dpi = 300)
-
-
-
-
-
 
 
 
